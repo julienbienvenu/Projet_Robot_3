e27c948c6e936c1c58369969006a0d2ce3d358d8
henri2h <henri.carnot@hotmail.fr>
2022-01-25 12:05:31 +0100

    fix: listen to responses

--- main.c
+++ main.c
@@ -158,7 +158,7 @@ volatile enum ZIGBEE Zigbee;
 
 uint8_t XBEE_RX[5];		  // les trames ZIGBEE sont en 5 bits
 uint8_t z_robotID = 0x5C; // the ID of the actual robot
-uint8_t z_tempo;	  // the actual temporisation before sending the robot ID
+volatile unsigned int z_tempo;	  // the actual temporisation before sending the robot ID
 #define MAX_RAND 1000 // max tempo: 2 seconds
 #define z_pos_increment                                                        \
 	50 // the value by wich each robot should be espaced // TODO : set me
